97bf1cd889b150f3bf063f2829dd3af3218ef7b2
Shaonty <shaonty.dutta@gmail.com>
2017-09-15 16:12:16 +0600

    add delivery model

--- core/models.py
+++ core/models.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class DeliveryDetail(models.Model):
+    PAYMENT_CHOICE = (
+        ('A', 'Advanced'),
+        ('C', 'COD'),
+    )
+    item_name = models.CharField(max_length=200)
+    item_details = models.TextField()
+    delivery_address = models.TextField()
+    payment_type = models.CharField(max_length=1, choices=PAYMENT_CHOICE)
+    payable_amount = models.IntegerField(default=0)
+    delivery_charge = models.IntegerField(default=0)
+    qr_code = models.ImageField(upload_to='qrcode', blank=True, null=True)
+
+
+
