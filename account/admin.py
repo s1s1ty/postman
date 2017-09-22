4c480265baad78e647f91bc27cf0a89b7fe10859
Shaonty <shaonty.dutta@gmail.com>
2017-09-22 12:19:42 +0600

    profile and core admin customize

--- account/admin.py
+++ account/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Profile
+from django.contrib.auth.models import Group
 
-admin.site.register(Profile)
+admin.site.unregister(Group)
+
--- core/admin.py
+++ core/admin.py
@@ -5,4 +5,14 @@ from django.contrib import admin
 
 from .models import SendProduct
 
-admin.site.register(SendProduct)
+
+class SendProductAdmin(admin.ModelAdmin):
+    list_display = ['user', 'item_name', 'delivery_address', 'payment_type', 'payable_amount', 'delivery_charge']
+    readonly_fields = ['user', 'item_name', 'item_details', 'delivery_address',
+                       'payment_type', 'payable_amount', 'delivery_charge', 'qr_code']
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+admin.site.register(SendProduct, SendProductAdmin)
+admin.site.disable_action('delete_selected')
